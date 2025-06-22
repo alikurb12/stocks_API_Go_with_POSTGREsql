2ba0dcf916dd12d86671aa43b9542839bdb9e52d
alikurb12 <kurbansahatowali@gmail.com>
2025-06-22 11:18:50 +0300

    Update server port

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/alikurb12/stocks_api_go/router"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/alikurb12/stocks_api_go/router"
 )
 
 func main() {
 	r := router.Router()
 	fmt.Println("Starting server on the port 8080......")
 
-	log.Fatal(http.ListenAndServe(":8008", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
